6b578f232928a1bd1ca7733ed613b588498f1c31
JHON GABRIEL OSPINA OROZCO <jhon.ospina@davivienda.com>
2025-06-12 23:18:38 -0500

    Se crea el endpoint pros-cons-discusser-stream, con el fin de retornar la respuesta que genera openia como un stream, esto me permite entregar al usuario la respuesta de openai, a medida que se va generando, con esto el usuario no debe esperar hasta que se genere la respuesta por completo si no que este podra ir leyendo la respuesta a medida que openia la va entregando

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { orthographyCheckUseCase } from './use-cases';
-import { OrthographyDto } from './dtos';
+import { orthographyCheckUseCase, prosConsDicusserStreamUseCase, prosConsDicusserUseCase } from './use-cases';
+import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
 import OpenAI from 'openai';
 
 @Injectable()
@@ -8,14 +8,21 @@ export class GptService {
 
 
     private openai = new OpenAI({
-       apiKey: process.env.OPENAI_API_KEY, 
-    })
+        apiKey: process.env.OPENAI_API_KEY,
+    });
     //Solo va a llamar casos de uso
 
     async orthographyCheck(orthographyDto: OrthographyDto) {
-       
         return await orthographyCheckUseCase(this.openai, {
             prompt: orthographyDto.prompt
-        })
+        });
+    }
+
+    async prosConsDicusser({ prompt }: ProsConsDiscusserDto) {
+        return await prosConsDicusserUseCase(this.openai, { prompt });
+    }
+
+    async prosConsDicusserStream({ prompt }: ProsConsDiscusserDto) {
+        return await prosConsDicusserStreamUseCase(this.openai, { prompt });
     }
 }
